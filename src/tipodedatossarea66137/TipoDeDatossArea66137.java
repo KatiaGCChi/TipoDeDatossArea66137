0f07c33c0713bed065a2aacd2c7f51d190e251f8
TRUJEQUE <TRUJEQUE@ANDREA>
2021-02-28 15:21:57 -0600

    area

---
+++ src/tipodedatossarea66137/TipoDeDatossArea66137.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatossarea66137;
+
+/**
+ *
+ * @author TRUJEQUE
+ */
+public class TipoDeDatossArea66137 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int area = 0 ;
+        int base = 15;
+        int altura = 25;
+        area = base * altura;
+        System.out.println("El area de un rectangulo de base "+base+" y altura "+altura+" es igual a "+area);
+        
+    }
+    
+}
